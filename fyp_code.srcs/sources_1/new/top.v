ba85286e5b86ba988496cc6cd0f73b933f61f5e2
Dora343 <wusiukwan31623159@gmail.com>
2024-01-22 20:16:05 +0800

    Create vivado project template

---
+++ fyp_code.srcs/sources_1/new/top.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/01/19 17:06:17
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+    input in,
+    input out,
+    inout ino
+    );
+    initial
+    begin 
+        //
+    end
+endmodule
