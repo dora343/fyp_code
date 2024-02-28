e992925db6055b7d0bd2846eef90eef457cb1dec
Dora343 <wusiukwan31623159@gmail.com>
2024-02-28 17:34:57 +0800

    Add clock to cmp

--- fyp_code.srcs/sources_1/new/comparator.v
+++ fyp_code.srcs/sources_1/new/comparator.v
@@ -1,9 +1,15 @@
 `timescale 1ns / 1ps
 
-module comparator(in1, in2, eq);
+module comparator(in1, in2, eq, clock);
     input [31:0] in1;
     input [31:0] in2;
+    input clock;
     output eq;
     
-    assign eq = (in1 == in2);
+    reg tmp;
+    always @(posedge clock)
+    begin
+        tmp <= in1 == in2;
+    end
+    assign eq = tmp;
 endmodule
